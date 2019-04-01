f973c18ab046fb6e05a04db4c3193e5aa1834b0d
siramok <sewellandres@gmail.com>
2019-04-01 14:42:04 -0600

    Completed 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,44 @@
-#write your code here
+def echo(say)
+    "#{say}"
+end
+
+def shout(say)
+    "#{say}".upcase
+end
+
+def repeat(*args)
+    if args.length == 1
+        return "#{args[0]} #{args[0]}"
+    end
+    if args.length == 2
+        return "#{args[0]} " * (args[1] - 1) + "#{args[0]}"
+    end
+end
+
+def start_of_word(*args)
+    if args.length == 1
+        return "#{args[0]}"[0]
+    end
+    if args.length == 2
+        return "#{args[0]}"[0.. args[1] - 1]
+    end
+end
+
+def first_word(word)
+    "#{word.partition(" ").first}"
+end
+
+def titleize(word)
+    new = ''
+    lw = ['and', 'the', 'over']
+    word.split.each_with_index do |w, i|
+        if i == 0
+            new += "#{w.capitalize} "
+        elsif lw.include?(w)
+            new += "#{w} "
+        else
+            new += "#{w.capitalize} "
+        end
+    end
+    return new.chomp(' ')
+end
