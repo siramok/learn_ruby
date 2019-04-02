ae016552d87f15f45c937883dc87fdd6da8e714b
siramok <sewellandres@gmail.com>
2019-04-02 11:52:19 -0600

    Completed 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+    def title=(new_title)
+        temp = ''
+        conj = ['a','an','and','in','or','of','the']
+        new_title.split.each_with_index do |w, i|
+            if i == 0
+                temp += "#{w.capitalize} "
+            elsif conj.include?(w)
+                temp += "#{w} "
+            else
+                temp += "#{w.capitalize} "
+            end
+        end
+        @title = temp.chomp(' ')
+    end
+    def title
+        @title
+    end
 end
