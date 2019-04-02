9457de0e46b8015e9f0e15936d16d07839dd3bae
siramok <sewellandres@gmail.com>
2019-04-02 13:27:00 -0600

    Completed 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+    @minutes = 0
+    @hours   = 0
+  end
+  def seconds=(new_sec)
+    if new_sec >= 3600
+      @hours = new_sec / 3600
+      new_sec -= @hours * 3600
+    end
+    if new_sec >= 60
+      @minutes = new_sec / 60
+      new_sec -= @minutes * 60
+    end
+    @seconds = new_sec
+  end
+  def padded(time)
+    format('%02d', time)
+  end
+  def time_string
+    "#{padded(@hours)}:#{padded(@minutes)}:#{padded(@seconds)}"
+  end
 end
